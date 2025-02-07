42490ac90699ecdb5f90cf821ecc9bb6a386e35c
Chyngyz88 <abykeevpythonch@gmail.com>
2025-02-07 17:33:30 +0600

    Feat: added GitHelp & main.py

---
+++ main.py
@@ -0,0 +1,57 @@
+# class def
+from setuptools.extern import names
+
+class Person:
+
+    #Это функция конструктор
+    def __init__(self, name, age):
+        # Атрибуты класса
+        self.name = name
+        self.age = age
+
+    # self - Это ссылка на OBJ или экземпляр класса
+    # Метод класса
+    def introduce(self,):
+        print(f"Hi i'm {self.name}")
+
+#class OBJ - Экземпляр класса
+ardager = Person("Ardager", 25)
+
+# ardager.introduce()
+
+# print(type(ardager))
+# print(type(123))
+# print(type("Hello"))
+
+# Родительский класс
+class Hero:
+
+    def __init__(self, name, hp, lvl):
+
+        self.name_1 = name
+        self.hp_1 = hp
+        self.lvl_1 = lvl
+
+    def action(self,): # метод героя
+        print(f"{self.name_1} делает базовое действие")
+
+# naofume = Hero("Naofume", 100, 3)
+#
+# Дочерний класс
+# class ShiledHero(Hero):
+#     pass # ---
+#     ... # ---
+#
+# naofume = ShiledHero("NaoFume", 100, 3)
+#
+# naofume.action()
+
+
+# class -- CamelCase
+# для переменных, методов, функций -- snek_case
+
+
+
+
+
+
